20b2d82c81747b7428796fbf0317e1b8959e612a
DraperStudio <hello@draperstud.io>
2015-02-25 11:53:28 +0100

    Big clean up

--- src/DropboxExtendSocialite.php
+++ src/DropboxExtendSocialite.php
@@ -5,8 +5,13 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class DropboxExtendSocialite
 {
+    /**
+     * Execute the provider.
+     */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
-        $socialiteWasCalled->extendSocialite('dropbox', __NAMESPACE__.'\Provider');
+        $socialiteWasCalled->extendSocialite(
+            'dropbox', __NAMESPACE__.'\Provider'
+        );
     }
 }
--- src/Provider.php
+++ src/Provider.php
@@ -12,7 +12,9 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase('https://www.dropbox.com/1/oauth2/authorize', $state);
+        return $this->buildAuthUrlFromBase(
+            'https://www.dropbox.com/1/oauth2/authorize', $state
+        );
     }
 
     /**
@@ -28,7 +30,8 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get('https://api.dropbox.com/1/account/info', [
+        $response = $this->getHttpClient()->get(
+            'https://api.dropbox.com/1/account/info', [
             'headers' => [
                 'Authorization' => 'Bearer '.$token,
             ],
@@ -43,11 +46,9 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'       => $user['uid'],
-            'nickname' => null,
-            'name'     => $user['display_name'],
-            'email'    => $user['email'],
-            'avatar'   => null,
+            'id' => $user['uid'], 'nickname' => null,
+            'name' => $user['display_name'], 'email' => $user['email'],
+            'avatar' => null,
         ]);
     }
 
@@ -56,6 +57,8 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getTokenFields($code)
     {
-        return array_merge(parent::getTokenFields($code), ['grant_type' => 'authorization_code']);
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
     }
 }
